d6f6f65b5850066985880cf887260194cc55428a
qhtneo <hj9671@gmail.com>
2024-01-16 20:03:33 +0900

    feat(Member) : domain, Repository, 비즈니스로직 구현

---
+++ src/main/java/com/example/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,41 @@
+package com.example.hellospring.repository;
+
+import com.example.hellospring.domain.Member;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static final Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        // null 이 반환될 가능성이 있기 때문에 Optional의 nullable사용
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        // findAny 는 Optional 로 반환
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
