24cd18ee3085224bcb146e3dea957fe00d767414
qhtneo <hj9671@gmail.com>
2024-01-17 11:10:57 +0900

    feat(Member): MemberController find 구현

--- src/main/java/com/example/hellospring/controller/MemberController.java
+++ src/main/java/com/example/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import com.example.hellospring.domain.Member;
 import com.example.hellospring.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -24,5 +27,12 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
